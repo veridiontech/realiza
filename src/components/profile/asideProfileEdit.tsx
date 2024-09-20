5242207ed3aa33679d7e863795d350804026fe33
levi.utima <levi.utima@gmail.com>
2024-09-20 18:13:59 -0300

    feat: implement aside in edit profile page

--- src/components/profile/asideProfileEdit.tsx
+++ src/components/profile/asideProfileEdit.tsx
@@ -1,39 +1,48 @@
 import { Bell, BriefcaseBusiness, Headset, MessageSquareText, Palette, SquareChevronLeft, UserPen } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 export function AsideProfileEdit() {
-    return(
-        <aside className="bg-white w-[308px] h-[1303px] flex flex-col items-start">
-            <div className="flex flex-row gap-4">
-            <SquareChevronLeft  className="text-[#2563EB]" cursor={'pointer'}/>
-             <span className="text-[#2563EB] text-[18px] font-medium">Confugurações</span>
+    return (
+        <aside className="bg-white w-[308px] h-[1303px] flex flex-col items-start  rounded-md">
+            <div className="flex flex-row gap-4 p-4">
+                <NavLink to='/profile'>
+                    <SquareChevronLeft className="text-[#2563EB]" cursor={'pointer'} />
+                </NavLink>
+                <span className="text-[#2563EB] text-[18px] font-medium">Configurações</span>
             </div>
-            <div className="bg-[#E3E3E3] w-[312px] border border"></div>
-            <div className="flex flex-col items-start justify-center gap-[30px]">
-                <div className="flex items-center justify-center">
-                    <UserPen />
-                    <span>Perfil</span>
-                </div>
-                <div  className="flex items-center justify-center">
-                    <BriefcaseBusiness />
-                    <span>Empresa</span>
-                </div>
-                <div  className="flex items-center justify-center">
-                    <MessageSquareText />
-                    <span>Mensagem</span>
-                </div>
-                <div  className="flex items-center justify-center">
-                    <Bell />
-                    <span>Notificação</span>
-                </div>
-                <div  className="flex items-center justify-center">
-                    <Palette />
-                    <span>Aparência</span>
-                </div>
-                <div  className="flex items-center justify-center">
-                    <Headset />
-                    <span>Suporte</span>
+
+            {/* Divider */}
+            <div className="bg-[#E3E3E3] w-full h-[1px]"></div>
+
+            {/* Conteúdo com padding aplicado sem afetar o divider */}
+            <div className="p-4 w-full">
+                <div className="flex flex-col items-start justify-center gap-[30px]">
+                    <div className="flex items-center justify-center gap-2">
+                        <UserPen />
+                        <span>Perfil</span>
+                    </div>
+                    <div className="flex items-center justify-center gap-2">
+                        <BriefcaseBusiness />
+                        <span>Empresa</span>
+                    </div>
+                    <div className="flex items-center justify-center gap-2">
+                        <MessageSquareText />
+                        <span>Mensagem</span>
+                    </div>
+                    <div className="flex items-center justify-center gap-2">
+                        <Bell />
+                        <span>Notificação</span>
+                    </div>
+                    <div className="flex items-center justify-center gap-2">
+                        <Palette />
+                        <span>Aparência</span>
+                    </div>
+                    <div className="flex items-center justify-center gap-2">
+                        <Headset />
+                        <span>Suporte</span>
+                    </div>
                 </div>
             </div>
         </aside>
-    )
+    );
 }
