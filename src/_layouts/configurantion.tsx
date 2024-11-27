d8285f5b6be67de00e0b6b18b8bb28da96f4b3a7
Jean <jean.castroaleixo@gmail.com>
2024-11-26 21:22:25 -0300

    feat: avatar in profile

--- src/_layouts/configurantion.tsx
+++ src/_layouts/configurantion.tsx
@@ -7,11 +7,11 @@ export function ConfigurationLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <Header />
-      <div className='bg-gray-200'>
-          <div className='flex h-46 w-1/5 bg-white mx-20 my-10 align-center'>
+      <div className='flex bg-gray-200'>
+          <div className='flex h-46 w-1/5 bg-white mx-12 my-10 '>
             <LateralMenu />
           </div>
-          <div className="flex bg-white">
+          <div className="flex h-screen w-2/3 mx-12 my-10 bg-white">
             <Outlet />
           </div>
       </div>
--- src/pages/auth/profileUser.tsx
+++ src/pages/auth/profileUser.tsx
@@ -1,6 +1,30 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
 export function ProfileUser () {
     return(
         <>
+        
+        <div className="flex flex-col w-full">
+            <h1 className="max-h-5 m-8">Meu Perfil</h1>
+            <div className="flex items-center w-full h-1/3 border-t border-b border-gray-300">
+            <Avatar className="ml-12 w-36 h-36">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+                <span className="ml-8">Jean de castro</span>
+                <span>span1</span>
+            </div>
+            <div className="justify-end">
+                <button>butao</button>
+            </div>
+            </div>
+            <div className="w-full h-2/5 "></div>
+            <div className="w-full h-1/3 border-t border-b border-gray-300"></div>
+        </div>
+
+
         </>
     )
 }
