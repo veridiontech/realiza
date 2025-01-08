af761e42a0f0b9b69847c07dd1adc3abbf97d26a
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2025-01-08 16:14:46 -0300

    Bugfix Lombok

--- api/src/main/java/bl/tech/realiza/gateways/repositories/clients/BranchRepository.java
+++ api/src/main/java/bl/tech/realiza/gateways/repositories/clients/BranchRepository.java
@@ -3,5 +3,10 @@ package bl.tech.realiza.gateways.repositories.clients;
 import bl.tech.realiza.domains.clients.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BranchRepository extends JpaRepository<Branch, String> {
+    Optional<Branch> findById(String id);
+    Branch save(Branch branch);
+    void deleteById(String id);
 }
--- api/src/main/java/bl/tech/realiza/usecases/impl/clients/CrudBranchImpl.java
+++ api/src/main/java/bl/tech/realiza/usecases/impl/clients/CrudBranchImpl.java
@@ -1,17 +1,45 @@
 package bl.tech.realiza.usecases.impl.clients;
 
+import bl.tech.realiza.domains.clients.Branch;
+import bl.tech.realiza.domains.clients.Client;
+import bl.tech.realiza.gateways.repositories.clients.BranchRepository;
+import bl.tech.realiza.gateways.repositories.clients.ClientRepository;
 import bl.tech.realiza.gateways.requests.clients.BranchRequestDto;
 import bl.tech.realiza.gateways.responses.clients.BranchResponseDto;
 import bl.tech.realiza.usecases.interfaces.clients.CrudBranch;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
+@RequiredArgsConstructor
 public class CrudBranchImpl implements CrudBranch {
+
+    private final BranchRepository branchRepository;
+    private final ClientRepository clientRepository;
+
     @Override
     public BranchResponseDto save(BranchRequestDto branchRequestDto) {
-        return null;
+
+        Optional<Client> clientOptional = clientRepository.findById(branchRequestDto.getClient());
+
+        Client client = clientOptional.orElseThrow(() -> new RuntimeException("Client not found"));
+
+        Branch newBranch = Branch.builder()
+                .name(branchRequestDto.getName())
+                .client(client)
+                .build();
+
+        Branch savedBranch = branchRepository.save(newBranch);
+
+        BranchResponseDto branchResponseDto = BranchResponseDto.builder()
+                .idBranch(savedBranch.getIdBranch())
+                .name(savedBranch.getName())
+                .client(savedBranch.getClient().getIdClient())
+                .build();
+        return branchResponseDto;
     }
 
     @Override
