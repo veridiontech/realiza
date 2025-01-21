19304f5a2b74f32f2b9d2bc826952d5dbd62f611
levi utima <levi.utima@gmail.com>
2025-01-21 10:47:48 -0300

    feat: implement new toast if send email and implement new select on modal

--- frontend/src/components/modal-teste-send-supplier.tsx
+++ frontend/src/components/modal-teste-send-supplier.tsx
@@ -1,78 +1,129 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog";
-  import { Button } from "./ui/button";
-  import { Label } from "./ui/label";
-  import { Input } from "./ui/input";
-  import { z } from "zod";
-  import { zodResolver } from "@hookform/resolvers/zod";
-  import { useForm } from "react-hook-form";
-  import axios from "axios";
-  import { ip } from "@/utils/ip";
-import { useClient } from "@/context/Client-Provider";
-  
-  const modalSendEmailFormSchema = z.object({
-    email: z.string(),
-    company: z.string().default('SUPPLIER')
-  })
-  
-  type ModalSendEmailFormSchema = z.infer<typeof modalSendEmailFormSchema>
-  export function ModalTesteSendSupplier() {
-    const { client } = useClient()
-  
-    const {
-      register,
-      handleSubmit
-    } = useForm<ModalSendEmailFormSchema>({
-      resolver: zodResolver(modalSendEmailFormSchema)
-    })
-  
-    const getIdUniqueClient = client?.idClient
-    
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { ip } from "@/utils/ip";
+import { useEffect, useState } from "react";
+import { Radio } from "react-loader-spinner";
+import { propsClient } from "@/types/interfaces";
+import { toast } from "sonner";
 
-    const createClient = async(data: ModalSendEmailFormSchema) => {
-      const payload = {
-        ...data,
-        idCompany: getIdUniqueClient
-      }
-      try{
-        await axios.post(`${ip}/invite`, payload)
-        console.log(payload);
-        
-        console.log('sucesso');
-        
-      }catch(err) {
-        console.log('erro ao enviar email para usuario', err);
-        
-      }
+const modalSendEmailFormSchema = z.object({
+  email: z.string(),
+  company: z.string().default("SUPPLIER"),
+  idCompany: z.string(),
+});
+
+type ModalSendEmailFormSchema = z.infer<typeof modalSendEmailFormSchema>;
+export function ModalTesteSendSupplier() {
+  const [clients, setClients] = useState<propsClient[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { register, handleSubmit } = useForm<ModalSendEmailFormSchema>({
+    resolver: zodResolver(modalSendEmailFormSchema),
+  });
+
+  const getClients = async () => {
+    try {
+      const res = await axios.get(`${ip}/client`);
+      setClients(res.data.content);
+      console.log(res.data.content);
+    } catch (err) {
+      console.log("erro ao puxar clientes", err);
     }
-  
-    return (
-      <Dialog>
-        <DialogTrigger asChild> 
-          <Button className="bg-sky-700">Cadastrar novo prestador</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Cadastrar novo cliente</DialogTitle>
-          </DialogHeader>
-          <div>
-            <form action="" onSubmit={handleSubmit(createClient)} className="flex flex-col gap-2">
-              <div>
-                <Label>Email</Label>
-                <Input type="email" placeholder="Digite o email do novo cliente" {...register("email")} className="w-full"/>
-              </div>
-              <div className="flex justify-end">
-              <Button className="bg-realizaBlue">Enviar</Button>
-              </div>
-            </form>
-          </div>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-  
+  };
+
+  const createClient = async (data: ModalSendEmailFormSchema) => {
+    console.log('enviando dados:',data);
+    setIsLoading(true)
+    try {
+      await axios.post(`${ip}/invite`, {
+        email: data.email,
+        idCompany: data.idCompany,
+        company: data.company
+      });
+      toast.success("Email de cadastro enviado para novo prestador")
+    } catch (err) {
+      console.log("erro ao enviar email para usuario", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getClients();
+  }, []);
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-sky-700">Cadastrar novo prestador</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Cadastrar novo prestador</DialogTitle>
+        </DialogHeader>
+        <div>
+          <form
+            action=""
+            onSubmit={handleSubmit(createClient)}
+            className="flex flex-col gap-4"
+          >
+            <div>
+              <Label>Email</Label>
+              <Input
+                type="email"
+                placeholder="Digite o email do novo cliente"
+                {...register("email")}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Label>Selecione um cliente</Label>
+              <select
+                className="rounded-md border p-2"
+                defaultValue=""
+                {...register("idCompany")}
+              >
+                <option value="" disabled>
+                  Selecione um cliente
+                </option>
+                {clients.map((client) => (
+                  <option key={client.idClient} value={client.idClient}>
+                    {client.companyName}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-end">
+              {isLoading ? (
+                <Button>
+                  <Radio
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="radio-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                  />
+                </Button>
+              ) : (
+                <Button className="bg-realizaBlue">Enviar</Button>
+              )}
+            </div>
+          </form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
