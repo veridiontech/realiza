828bb8b3a4b93f2b1aa97ff0fe900fdc196ccce2
jean <jean.castroaleixo@gmail.com>
2025-01-27 15:38:28 -0300

    adicionei o context em todas as paginas

--- frontend/src/hooks/gets/useEmployees.ts
+++ frontend/src/hooks/gets/useEmployees.ts
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Employee } from "@/types/employee";
 import { ip } from "@/utils/ip";
 
-const API_URL = `${ip}/employee/brazilian`;
+const API_URL = `${ip}/employee`;
 
 export function useEmployees() {
   const [employees, setEmployees] = useState<Employee[]>([]);
@@ -11,12 +11,17 @@ export function useEmployees() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  async function fetchEmployees(limit = 10, page = 0) {
+  async function fetchEmployees(
+    limit = 10,
+    page = 0,
+    enterprise = "CLIENT",
+    idSearch = "",
+  ) {
     setLoading(true);
     setError(null);
     try {
       const response = await axios.get(API_URL, {
-        params: { size: limit, page },
+        params: { size: limit, page, enterprise, idSearch },
       });
 
       const { content, totalPages } = response.data;
