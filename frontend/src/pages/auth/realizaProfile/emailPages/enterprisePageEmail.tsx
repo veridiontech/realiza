3afdf284366ed6ac7a1b32f51b383fb15f56a542
levi utima <levi.utima@gmail.com>
2025-02-03 16:27:26 -0300

    feat: implement cnpj in enterprise signup email

--- frontend/src/pages/auth/realizaProfile/emailPages/enterprisePageEmail.tsx
+++ frontend/src/pages/auth/realizaProfile/emailPages/enterprisePageEmail.tsx
@@ -2,11 +2,12 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFormDataContext } from "@/context/formDataProvider";
-// import { ip } from "@/utils/ip";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { Oval } from "react-loader-spinner";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { z } from "zod";
 
@@ -18,66 +19,104 @@ const enterprisePageEmailFormSchema = z.object({
   phone: z.string().nonempty("O telefone é obrigatório"),
 });
 
-type EnterprisePageEmailFormSchema = z.infer<typeof enterprisePageEmailFormSchema>;
-export function EnterprisePageEmail() {
-    const {setEnterpriseData} = useFormDataContext()
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams();
-    const token = searchParams.get("token");
-    const [ isValidToken, setIsValidToken ] = useState(false)
-    const findIdCompany = searchParams.get("id")
-    const findCompany = searchParams.get("company")
+type EnterprisePageEmailFormSchema = z.infer<
+  typeof enterprisePageEmailFormSchema
+>;
 
+export function EnterprisePageEmail() {
+  const { setEnterpriseData } = useFormDataContext();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+  const [isValidToken, setIsValidToken] = useState(false);
+  const findIdCompany = searchParams.get("id");
+  const findCompany = searchParams.get("company");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const validateToken = async() => {
-      try{ 
-        const res = await axios.get(`https://realiza-1.onrender.com/email/Enterprise-sign-up/validate?token=${token}`)
-        if(res.status === 200) {
-          setIsValidToken(true)
-        }else {
-          console.log('erro');
+    const validateToken = async () => {
+      try {
+        const res = await axios.get(
+          `https://realiza-1.onrender.com/email/Enterprise-sign-up/validate?token=${token}`,
+        );
+        if (res.status === 200) {
+          setIsValidToken(true);
+        } else {
+          console.log("Erro ao validar token.");
         }
-      }catch(err) {
-        console.log("Nao foi possivel validar o token", err);
-        setIsValidToken(false)
+      } catch (err) {
+        console.log("Não foi possível validar o token", err);
+        setIsValidToken(false);
       }
-    }
-    if(token) {
-      validateToken()
+    };
+    if (token) {
+      validateToken();
     } else {
-      console.log("nao foi possivel verificar o token");
+      console.log("Token não encontrado.");
     }
-  }, [token])
+  }, [token]);
 
   const {
     register,
     handleSubmit,
+    setValue,
+    getValues,
     formState: { isValid },
   } = useForm<EnterprisePageEmailFormSchema>({
     resolver: zodResolver(enterprisePageEmailFormSchema),
-    mode: "onChange"
+    mode: "onChange",
   });
 
+  const validateCnpj = async () => {
+    setIsLoading(true);
+    const cnpj = getValues("cnpj").replace(/\D/g, "");
+    if (cnpj.length !== 14) return;
+
+    try {
+      const res = await axios.get(
+        `https://www.receitaws.com.br/v1/cnpj/${cnpj}`,
+      );
+
+      console.log(res.data);
+
+      if (res.data) {
+        setValue("socialReason", res.data.nome);
+        setValue("fantasyName", res.data.fantasia || "Sem nome fantasia");
+        setValue("email", res.data.email);
+        setValue("phone", res.data.telefone);
+      }
+    } catch (err) {
+      console.log("Erro ao buscar CNPJ:", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const onSubmit = async (data: EnterprisePageEmailFormSchema) => {
+    setIsLoading(true);
     try {
       const payload = {
         ...data,
         idCompany: findIdCompany || "",
         company: findCompany,
-      }
-      console.log("teste",payload);
-      
+      };
+      console.log("Enviando dados:", payload);
+
       setEnterpriseData(payload);
-      console.log("Cadastro realizado:", data);
       navigate(`/email/Sign-up`);
     } catch (err) {
       console.error("Erro ao enviar os dados:", err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   if (!isValidToken) {
-    return <div className="text-red-600">Token inválido ou expirado. Por favor, solicite um novo convite.</div>;
+    return (
+      <div className="text-red-600">
+        Token inválido ou expirado. Por favor, solicite um novo convite.
+      </div>
+    );
   }
 
   return (
@@ -89,50 +128,94 @@ export function EnterprisePageEmail() {
         <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex items-center gap-5">
             <div>
-              <Label>Nome fantasia</Label>
+              <Label>CNPJ</Label>
+              <div className="flex items-center gap-2">
+                <Input
+                  type="text"
+                  placeholder="CNPJ: __.___.___/____-__"
+                  className="w-[10vw]"
+                  {...register("cnpj")}
+                />
+                {isLoading ? (
+                  <Button type="button" onClick={validateCnpj}>
+                    <Oval
+                      visible={true}
+                      height="80"
+                      width="80"
+                      color="#4fa94d"
+                      ariaLabel="oval-loading"
+                      wrapperStyle={{}}
+                      wrapperClass=""
+                    />
+                  </Button>
+                ) : (
+                  <Button type="button" onClick={validateCnpj}>
+                    <Search />
+                  </Button>
+                )}
+              </div>
+            </div>
+            <div>
+              <Label>Telefone</Label>
               <Input
                 type="text"
-                placeholder="Nome Fantasia"
+                placeholder="Telefone"
                 className="w-[13vw]"
-                {...register('fantasyName')}
+                {...register("phone")}
               />
             </div>
           </div>
+
           <div>
             <Label>Email corporativo</Label>
             <Input
               type="email"
               placeholder="Digite o seu email"
               className="w-[27vw]"
-              {...register('email')}
-            />
-          </div>
-          <div>
-            <Label>Razão social</Label>
-            <Input
-              type="text"
-              placeholder="*Razão social"
-              className="w-[27vw]"
-              {...register('socialReason')}
+              {...register("email")}
             />
           </div>
           <div className="flex items-center gap-5">
             <div>
-              <Label>Telefone</Label>
-              <Input type="text" placeholder="Telefone" className="w-[13vw]" 
-              {...register('phone')}/>
-            </div>
-            <div>
-              <Label>Cnpj</Label>
+              <Label>Nome fantasia</Label>
               <Input
                 type="text"
-                placeholder="CNPJ: __.___.___/____-__"
+                placeholder="Nome Fantasia"
                 className="w-[13vw]"
-                {...register('cnpj')}
+                {...register("fantasyName")}
+              />
+            </div>
+            <div>
+              <Label>Razão social</Label>
+              <Input
+                type="text"
+                placeholder="*Razão social"
+                className="w-[13vw]"
+                {...register("socialReason")}
               />
             </div>
           </div>
-          <Button className="bg-realizaBlue h-[5vh]" type="submit" disabled={!isValid}>Cadastrar empresa</Button>
+          {isLoading ? (
+            <Button className="bg-realizaBlue h-[5vh]">
+              <Oval
+                visible={true}
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="oval-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+              />
+            </Button>
+          ) : (
+            <Button
+              className="bg-realizaBlue h-[5vh]"
+              type="submit"
+              disabled={!isValid}
+            >
+              Cadastrar empresa
+            </Button>
+          )}
         </form>
       </div>
     </div>
