0ec03346d50ba33e6c4621be7d0623d952685b80
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2025-02-06 12:13:26 -0300

    bugfix Render variables

--- api/src/main/java/bl/tech/realiza/RealizaApplication.java
+++ api/src/main/java/bl/tech/realiza/RealizaApplication.java
@@ -1,29 +1,25 @@
 package bl.tech.realiza;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RealizaApplication {
 
+	@Autowired
+	private Dotenv dotenv;  // Usa o bean DotenvConfig
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-		Dotenv dotenv = Dotenv.configure()
-				.directory("./")      // Busca o .env no diretório atual
-				.ignoreIfMissing()    // Ignora se o .env não for encontrado
-				.load();
-
-		// Adiciona as variáveis do .env às variáveis de ambiente do sistema
+		// Definir variáveis do .env como propriedades do sistema
 		dotenv.entries().forEach(entry -> {
-			// Se a variável do sistema já existir, não sobrescreve
-			System.setProperty(entry.getKey(),
-					System.getenv(entry.getKey()) != null
-							? System.getenv(entry.getKey())
-							: entry.getValue());
+			if (System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
 		});
-
-
 		SpringApplication.run(RealizaApplication.class, args);
 	}
 
