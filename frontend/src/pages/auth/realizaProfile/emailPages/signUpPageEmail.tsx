9c8a1f725bf86a1d9fd150a41520da8442059d7c
levi utima <levi.utima@gmail.com>
2025-02-11 16:32:41 -0300

    fix: change role in create user client

--- frontend/src/pages/auth/realizaProfile/emailPages/signUpPageEmail.tsx
+++ frontend/src/pages/auth/realizaProfile/emailPages/signUpPageEmail.tsx
@@ -19,7 +19,7 @@ const signUpEmailFormSchema = z
     cpf: z.string().nonempty("Cpf é obrigatório"),
     email: z.string().email("Formato de email inválido").nonempty("Email é obrigatório"),
     position: z.string().nonempty("Seu cargo é obrigatório"),
-    role: z.string().default("ROLE_CLIENT_RESPONSABLE"),
+    role: z.string().default("ROLE_CLIENT_RESPONSIBLE"),
     password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
     confirmPassword: z.string().min(6, "Confirmação de senha deve ter pelo menos 6 caracteres"),
   })
