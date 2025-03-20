032cdc7aa03b0774101cedcda9fa9553461429cf
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2025-03-20 20:04:36 -0300

    correction registration flow

--- api/src/main/java/bl/tech/realiza/domains/providers/Provider.java
+++ api/src/main/java/bl/tech/realiza/domains/providers/Provider.java
@@ -21,12 +21,12 @@ public abstract class Provider {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String idProvider;
     @Column(unique = true)
-    private String cnpj;
+    private String cnpj; //
     private String tradeName;
-    private String corporateName;
+    private String corporateName; //
     private String logo;
-    private String email;
-    private String telephone;
+    private String email; //
+    private String telephone; //
     private String cep;
     private String state;
     private String city;
