49ab3be08c298cc9fc75c5a4a54aea7441f524d2
Vinicius <viniciusantos.eira@gmail.com>
2025-03-24 15:48:26 -0300

    fix: Perfil empresarial fornecedores/sucontratados

--- frontend/src/components/header/providerLateralMenu.tsx
+++ frontend/src/components/header/providerLateralMenu.tsx
@@ -17,6 +17,85 @@ export function ProviderLateralMenu() {
 
   const getIdUser = user?.idUser;
 
+  if(user?.role === "ROLE_SUBCONTRACTOR_RESPONSIBLE") {
+    return (
+      <SheetContent className="h-full overflow-auto dark:bg-white" side={"left"}>
+      <Link to={`/fornecedor/home/${getIdUser}`}>
+      <Button
+        variant={"ghost"}
+        className="w-full justify-start bg-zinc-100 px-4 py-2"
+      >
+        <Home className="text-realizaBlue size-4" />
+        <span className="ml-2 text-sm font-medium text-zinc-900">Início</span>
+      </Button>
+      </Link>
+      <h3 className="pl-4 pt-6 text-xs text-zinc-800">
+        Prestadores de serviço
+      </h3>
+      <Link to={`contracts/${user?.idUser}`}>
+        <Button
+          variant={"ghost"}
+          className="mt-2 w-full justify-start px-4 py-2"
+        >
+          <Users2 className="size-4 text-zinc-800" />
+          <span className="ml-2 text-sm font-medium text-zinc-900">
+            Ver prestadores
+          </span>
+        </Button>
+      </Link>
+      <Link to={`/fornecedor/dashboard/${getIdUser}`}>
+        <Button
+          variant={"ghost"}
+          className="mt-1 w-full justify-start px-4 py-2"
+        >
+          <ChartPie className="size-4 text-zinc-800" />
+          <span className="ml-2 text-sm font-medium text-zinc-900">
+            Ver BI’s
+          </span>
+        </Button>
+      </Link>
+      <h3 className="pl-4 pt-6 text-xs text-zinc-800">
+        Colaboradores e contratos
+      </h3>
+      <Link to={`contracts/${user?.idUser}`}>
+        <Button
+          variant={"ghost"}
+          className="mt-1 w-full justify-start px-4 py-2"
+        >
+          <File className="size-4 text-zinc-800" />
+          <span className="ml-2 text-sm font-medium text-zinc-900">
+            Meus contratos
+          </span>
+        </Button>
+      </Link>
+      <h3 className="pl-4 pt-6 text-xs text-zinc-800">Sobre a empresa</h3>
+      <Link to={`/fornecedor/profile/${getIdUser}`}>
+        <Button
+          variant={"ghost"}
+          className="mt-2 w-full justify-start px-4 py-2"
+        >
+          <Building2 className="size-4 text-zinc-800" />
+          <span className="ml-2 text-sm font-medium text-zinc-900">
+            Empresa
+          </span>
+        </Button>
+      </Link>
+
+      <Link to={`/fornecedor/employees/${getIdUser}`}>
+        <Button
+          variant={"ghost"}
+          className="mt-2 w-full justify-start px-4 py-2"
+        >
+          <PersonStanding className="size-4 text-zinc-800" />
+          <span className="ml-2 text-sm font-medium text-zinc-900">
+            Colaboradores
+          </span>
+        </Button>
+      </Link>
+    </SheetContent>
+    )
+  }
+
   return (
     <SheetContent className="h-full overflow-auto dark:bg-white" side={"left"}>
       <Link to={`/fornecedor/home/${getIdUser}`}>
--- frontend/src/pages/auth/realizaProfile/profileEnterprise/__profile-enterprise.tsx
+++ frontend/src/pages/auth/realizaProfile/profileEnterprise/__profile-enterprise.tsx
@@ -1,8 +1,13 @@
-import { Mail, Phone } from "lucide-react";
+import { Locate, Mail, Phone } from "lucide-react";
 import { Helmet } from "react-helmet-async";
 import { EditModalEnterprise } from "./edit-modal-enterprise";
 import { useClient } from "@/context/Client-Provider";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useSupplier } from "@/context/Supplier-context";
+// import axios from "axios";
+// import { ip } from "@/utils/ip";
+// import { useEffect, useState } from "react";
+import { useUser } from "@/context/user-provider";
 // import axios from "axios";
 // import { ip } from "@/utils/ip";
 // import { useEffect } from "react";
@@ -28,6 +33,9 @@ export function ProfileEnterpriseReprise() {
   // const [selectedFile, setSelectedFile] = useState<File | null>(null);
   // const [logoClient, setLogoClient] = useState<string | null>(null);
   const { client } = useClient();
+  const { supplier } = useSupplier();
+  const { user } = useUser();
+  // const [supplierData, setSupplierData] = useState(null);
   const firstLetter = client?.tradeName?.charAt(0) || "";
   const lastLetter = client?.tradeName?.slice(-1) || "";
 
@@ -68,6 +76,98 @@ export function ProfileEnterpriseReprise() {
   // useEffect(() => {
   //   getLogoClient();
   // });
+
+  // const getSupplier = async () => {
+  //   if (!supplier?.idProvider) return; 
+  //   try {
+  //     const res = await axios.get(`${ip}/supplier/${supplier.idProvider}`);
+  //     setSupplierData(res.data);
+  //   } catch (error) {
+  //     console.error("Erro ao buscar fornecedor:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if( supplier?.idProvider) {
+  //     getSupplier()
+  //   }
+  // }, [supplier?.idProvider])
+
+  // console.log(user)
+
+  if (user?.role === "ROLE_SUPPLIER_RESPONSIBLE") {
+    return (
+      <>
+        <Helmet title="profile" />
+        <section className="mx-4 flex flex-col md:mx-8 lg:mx-20">
+          <div className="dark:bg-primary mb-10 rounded-t-xl bg-white">
+            <div className="bg-realizaBlue min-h-[220px] w-full rounded-t-xl" />
+            <div className="shadow-custom-blue relative flex w-full flex-col px-4 pb-10 sm:px-6 md:px-8 lg:px-12">
+              <div className="flex flex-col items-center justify-between md:flex-row">
+                <div className="relative bottom-10 left-1 flex items-center gap-4">
+                  <div className="bg-realizaBlue flex h-[16vh] w-[8vw] items-center justify-center rounded-full p-7">
+                    <div className="text-[40px] text-white">
+                      {firstLetter}
+                      {lastLetter}
+                    </div>
+                  </div>
+                  <div className="relative top-5 flex flex-col gap-2 md:gap-5 dark:text-white">
+                    <div className="text-lg font-medium text-sky-800">
+                      {supplier?.tradeName ? (
+                        <h2>{supplier.tradeName}</h2>
+                      ) : (
+                        <span className="font-normal">
+                          Nenhum fornecedor selecionado
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <div>
+                        {supplier?.tradeName ? (
+                          <span className="text-xs font-medium text-sky-700">
+                            {supplier.tradeName} / {supplier?.cnpj}
+                          </span>
+                        ) : (
+                          <div className="flex items-center gap-2">
+                            <Skeleton className="h-[1vh] w-[4vw]" />
+                            <span className="text-sky-700"> / </span>
+                            <Skeleton className="h-[1vh] w-[4vw]" />
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <EditModalEnterprise />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-8 lg:flex-row">
+            <div className="shadow-custom-blue mb-10 flex flex-1 flex-col gap-6 bg-white px-4 py-5 md:px-6 md:py-6">
+              <h2 className="text-lg font-medium">Formas de contato</h2>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-row items-center gap-2">
+                  <Mail />
+                  <span>E-mail: {supplier?.email || "Não disponível"}</span>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                    <Locate/>
+                  <span className="flex items-center gap-2">
+                    <span>Cep: </span>
+                    <span>{supplier?.cep || "Não disponível"}</span>
+                  </span>
+                </div>
+              </div>
+              <span>Outras formas de contato</span>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+  
 
   return (
     <>
