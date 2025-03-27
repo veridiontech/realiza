0d1ec3efff4a2b01de65a4e066847c96d288439b
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2025-03-27 16:09:46 -0300

    bugfix painel de controle

--- api/src/main/java/bl/tech/realiza/gateways/requests/services/itemManagement/ItemManagementUserRequestDto.java
+++ api/src/main/java/bl/tech/realiza/gateways/requests/services/itemManagement/ItemManagementUserRequestDto.java
@@ -1,8 +1,10 @@
 package bl.tech.realiza.gateways.requests.services.itemManagement;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ItemManagementUserRequestDto {
     private String title;
     private String details;
