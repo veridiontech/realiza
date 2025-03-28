4838958d11a57cc03b0160f5dd37b32eaebc869a
leviutima <levi.utima@gmail.com>
2025-03-28 09:04:23 -0300

    feat: implement push datas branch

--- frontend/src/pages/auth/realizaProfile/branchs/branch.tsx
+++ frontend/src/pages/auth/realizaProfile/branchs/branch.tsx
@@ -1,149 +1,29 @@
-// import { useEffect, useState } from "react";
-// import { SearchCnpjModal } from "./modals/newBranchStepOne";
-// import { NewBranchModal } from "./modals/newBranchStepTwo";
-// import { ButtonBlue } from "@/components/ui/buttonBlue";
-// import { Pagination } from "@/components/ui/pagination";
-// import { Table } from "@/components/ui/tableVanila";
-// import axios from "axios";
-// import { Puff } from "react-loader-spinner";
-// import { ip } from "@/utils/ip";
-// import { useClient } from "@/context/Client-Provider";
-
-// interface BranchType {
-//   idBranch: string;
-//   name: string; // Atualizado para refletir o campo "name"
-//   cnpj: string;
-//   address: string;
-//   acoes: string;
-// }
-
-// const columns: {
-//   key: keyof BranchType;
-//   label: string;
-//   render?: (value: any, row: BranchType) => JSX.Element;
-// }[] = [
-//   { key: "name", label: "Nome da Filial" }, // Alterado para "name"
-//   { key: "cnpj", label: "CNPJ" },
-//   { key: "address", label: "Endereço" },
-//   { key: "acoes", label: "Ações"},
-// ];
-
-// export function Branch() {
-//   const [branches, setBranches] = useState<BranchType[]>([]);
-//   const [totalPages, setTotalPages] = useState(1);
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
-//   const [isNewBranchModalOpen, setIsNewBranchModalOpen] = useState(false);
-//   const [selectedCnpj, setSelectedCnpj] = useState<string | null>(null);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState<string | null>(null);
-//   const {client} = useClient()
-
-//   //   const itemsPerPage = 10;
-
-//   const fetchBranches = async () => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await axios.get(`${ip}/branch/filtered-client?idSearch=${client?.idClient}`);
-//       const { content, totalPages: total } = response.data;
-//       setBranches(content);
-//       setTotalPages(total);
-//       console.log(content);
-//     } catch (err) {
-//       console.error("Erro ao buscar filiais:", err);
-//       setError("Erro ao buscar filiais. Tente novamente.");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (client?.idClient) {
-//       setBranches([]);
-//       setCurrentPage(1);
-//       fetchBranches();
-//     }
-//   }, [client?.idClient]);
-//   const handlePageChange = (page: number) => {
-//     if (page >= 1 && page <= totalPages) {
-//       setCurrentPage(page);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div className="m-4 flex justify-center">
-//         <div className="flex w-[90rem] flex-col rounded-lg bg-white p-4 shadow-md">
-//           <div className="mb-6 flex items-center justify-between">
-//             <h1 className="m-8 text-2xl">Filiais</h1>
-//             <ButtonBlue onClick={() => setIsSearchModalOpen(true)}>
-//               Adicionar Filial
-//             </ButtonBlue>
-//           </div>
-//           {loading ? (
-//             <div className="flex w-[20vw] items-center justify-start rounded-md border p-2 dark:bg-white">
-//               <Puff
-//                 visible={true}
-//                 height="30"
-//                 width="30"
-//                 color="#34495D"
-//                 ariaLabel="puff-loading"
-//               />
-//               <span className="ml-2 text-black">Carregando...</span>
-//             </div>
-//           ) : error ? (
-//             <div className="text-center text-red-500">{error}</div>
-//           ) : (
-//             <Table<BranchType> data={branches} columns={columns} />
-//           )}
-//           <Pagination
-//             currentPage={currentPage}
-//             totalPages={totalPages}
-//             onPageChange={handlePageChange}
-//           />
-//         </div>
-//       </div>
-
-//       {isSearchModalOpen && (
-//         <SearchCnpjModal
-//           onClose={() => setIsSearchModalOpen(false)}
-//           onProceed={(cnpj) => {
-//             setSelectedCnpj(cnpj);
-//             setIsSearchModalOpen(false);
-//             setIsNewBranchModalOpen(true);
-//           }}
-//         />
-//       )}
-
-//       {isNewBranchModalOpen && selectedCnpj && (
-//         <NewBranchModal
-//           onClose={() => setIsNewBranchModalOpen(false)}
-//           onSubmit={(data) => {
-//             console.log("Dados enviados:", data);
-//             setIsNewBranchModalOpen(false);
-//           }}
-//         />
-//       )}
-//     </div>
-//   );
-// }
-
 import { useEffect, useState } from "react";
-import { SearchCnpjModal } from "./modals/newBranchStepOne";
-import { NewBranchModal } from "./modals/newBranchStepTwo";
-import { ButtonBlue } from "@/components/ui/buttonBlue";
 import { Pagination } from "@/components/ui/pagination";
 import { Table } from "@/components/ui/tableVanila";
 import axios from "axios";
 import { Puff } from "react-loader-spinner";
 import { ip } from "@/utils/ip";
 import { useClient } from "@/context/Client-Provider";
-import { Eye } from "lucide-react"; // Importando apenas o ícone Eye
+import { Eye, Search } from "lucide-react"; // Importando apenas o ícone Eye
 import { Link } from "react-router-dom";
 import { useUser } from "@/context/user-provider";
 import { useBranch } from "@/context/Branch-provider";
 import { propsBranch } from "@/types/interfaces";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { Label } from "@/components/ui/label";
 
 // interface BranchType {
 //   idBranch: string;
@@ -153,18 +33,40 @@ import { propsBranch } from "@/types/interfaces";
 //   actions: string;
 // }
 
+const newBranchFormSchema = z.object({
+  cnpj: z.string(),
+  name: z.string().min(1, "O nome fantasia é obrigatório."),
+  email: z.string().email("Insira um email válido"),
+  cep: z.string().min(8, "O CEP deve ter pelo menos 8 caracteres."),
+  country: z.string().min(1, "O país é obrigatório."),
+  state: z.string().min(1, "O estado é obrigatório."),
+  city: z.string().min(1, "A cidade é obrigatória."),
+  address: z.string().min(1, "O endereço é obrigatório."),
+  number: z.string().nonempty("Número é obrigatório"),
+  telephone: z.string().nonempty("Insira um telefone")
+});
+
+type NewBranchFormSchema = z.infer<typeof newBranchFormSchema>;
 export function Branch() {
   const [branches, setBranches] = useState<propsBranch[]>([]);
   const [totalPages, setTotalPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
-  const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
-  const [isNewBranchModalOpen, setIsNewBranchModalOpen] = useState(false);
-  const [selectedCnpj, setSelectedCnpj] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const { client } = useClient();
   const { user } = useUser();
-  const {setSelectedBranch} = useBranch()
+  const { setSelectedBranch } = useBranch();
+  const [razaoSocial, setRazaoSocial] = useState<string | null>(null);
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm<NewBranchFormSchema>({
+    resolver: zodResolver(newBranchFormSchema),
+  });
 
   const columns: {
     key: keyof propsBranch;
@@ -184,7 +86,10 @@ export function Branch() {
               <Eye />
             </Link>
           ) : (
-            <Link to={`/sistema/profileBranch/${row.idBranch}`} onClick={() => setSelectedBranch(row)}>
+            <Link
+              to={`/sistema/profileBranch/${row.idBranch}`}
+              onClick={() => setSelectedBranch(row)}
+            >
               <Eye />
             </Link>
           )}
@@ -211,6 +116,47 @@ export function Branch() {
     }
   };
 
+  const handleCnpj = async () => {
+    const cnpj = getValues("cnpj").replace(/\D/g, "");
+    if (cnpj.length !== 14) {
+      toast.error("CNPJ inválido");
+      return;
+    }
+    try {
+      const res = await axios.get(`https://open.cnpja.com/office/${cnpj}`);
+      console.log("Dados da empresa:", res.data);
+      const razao =
+        res.data.company.name ||
+        res.data.company.name ||
+        "Razão social não encontrada";
+
+      const cep = res.data.address.zip;
+
+      const city = res.data.address.city;
+
+      const address = res.data.address.street;
+
+      const country = res.data.address.country.name;
+
+      const state = res.data.address.state;
+      const number = res.data.address.number
+
+      setValue("number", number)
+      setValue("state", state);
+      setValue("country", country);
+      setValue("address", address);
+      setValue("cep", cep);
+      setValue("city", city);
+      setValue("name", razao);
+      setRazaoSocial(razao);
+      toast.success("Sucesso ao buscar CNPJ");
+    } catch (err) {
+      setRazaoSocial(null);
+      toast.error("Erro ao buscar CNPJ");
+      console.error("Erro ao buscar CNPJ:", err);
+    }
+  };
+
   useEffect(() => {
     if (client?.idClient) {
       setBranches([]);
@@ -224,6 +170,38 @@ export function Branch() {
       setCurrentPage(page);
     }
   };
+  
+  const onSubmit = async (data: NewBranchFormSchema) => {
+    const payload = {
+      ...data,
+      client: client?.idClient,
+    };
+  
+    try {
+      console.log("enviando dados:", payload);
+      await axios.post(`${ip}/branch`, payload);
+      toast.success("Sucesso ao criar filial");
+    } catch (err: any) {
+      // Se o erro vier com resposta do backend
+      if (err.response && err.response.data) {
+        const mensagemBackend =
+          err.response.data.message ||
+          err.response.data.error ||
+          "Erro inesperado no servidor";
+        console.log(mensagemBackend);
+        
+        toast.error(mensagemBackend);
+      } else if (err.request) {
+        // Erro de conexão
+        toast.error("Não foi possível se conectar ao servidor.");
+      } else {
+        // Erro desconhecido
+        toast.error("Erro desconhecido ao processar requisição.");
+      }
+  
+      console.error("Erro ao criar filial:", err);
+    }
+  };
 
   return (
     <div>
@@ -231,9 +209,78 @@ export function Branch() {
         <div className="flex w-[90rem] flex-col rounded-lg bg-white p-4 shadow-md">
           <div className="mb-6 flex items-center justify-between">
             <h1 className="m-8 text-2xl">Filiais</h1>
-            <ButtonBlue onClick={() => setIsSearchModalOpen(true)}>
-              Adicionar Filial
-            </ButtonBlue>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="bg-realizaBlue">Adicionar Filial</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Cadastro de filial</DialogTitle>
+                </DialogHeader>
+                <form action="" onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                  <div className="flex flex-col gap-2">
+                  <div className="flex flex-col gap-2">
+                    <Label>CNPJ</Label>
+                    <div className="flex items-center gap-2">
+                      <Input type="text" {...register("cnpj")} />
+                      <div
+                        onClick={handleCnpj}
+                        className="bg-realizaBlue cursor-pointer rounded-lg p-2 hover:bg-gray-500"
+                      >
+                        <Search className="text-white" />
+                      </div>
+                    </div>
+                    {razaoSocial && (
+                      <p className="mt-1 text-sm text-gray-700">
+                        Razão social: <strong>{razaoSocial}</strong>
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <Label>Email</Label>
+                    <Input type="email" {...register("email")}/>
+                  </div>
+                  <div>
+                    <Label>Razão social</Label>
+                    <Input type="text" {...register("name")} />
+                  </div>
+                  <div>
+                    <Label>CEP</Label>
+                    <Input type="text" {...register("cep")} />
+                  </div>
+                  <div>
+                    <Label>Cidade</Label>
+                    <Input type="text" {...register("city")} />
+                  </div>
+                  <div>
+                  <div>
+                    <Label>Endereço</Label>
+                    <Input type="text" {...register("address")} />
+                  </div>
+                  <div>
+                    <Label>Número</Label>
+                    <Input type="text" {...register("number")}/>
+                  </div>
+                  </div>
+                  <div>
+                    <Label>País</Label>
+                    <Input type="text" {...register("country")} />
+                  </div>
+                  <div>
+                    <Label>Estado</Label>
+                    <Input type="text" {...register("state")} />
+                  </div>
+                  <div>
+                    <Label>Telefone</Label>
+                    <Input type="text" {...register("telephone")}/>
+                  </div>
+                  </div>
+                  <div className="flex justify-end">
+                    <Button>Cadastrar</Button>
+                  </div>
+                </form>
+              </DialogContent>
+            </Dialog>
           </div>
           {loading ? (
             <div className="flex w-[20vw] items-center justify-start rounded-md border p-2 dark:bg-white">
@@ -258,27 +305,6 @@ export function Branch() {
           />
         </div>
       </div>
-
-      {isSearchModalOpen && (
-        <SearchCnpjModal
-          onClose={() => setIsSearchModalOpen(false)}
-          onProceed={(cnpj) => {
-            setSelectedCnpj(cnpj);
-            setIsSearchModalOpen(false);
-            setIsNewBranchModalOpen(true);
-          }}
-        />
-      )}
-
-      {isNewBranchModalOpen && selectedCnpj && (
-        <NewBranchModal
-          onClose={() => setIsNewBranchModalOpen(false)}
-          onSubmit={(data) => {
-            console.log("Dados enviados:", data);
-            setIsNewBranchModalOpen(false);
-          }}
-        />
-      )}
     </div>
   );
 }
