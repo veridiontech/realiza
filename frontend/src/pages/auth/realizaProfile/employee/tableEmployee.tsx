849046e2393d11746dc691192bd595fb93d2c9e3
leviutima <levi.utima@gmail.com>
2025-05-08 19:18:21 -0300

    feat: loading and search bar employee

--- frontend/src/pages/auth/realizaProfile/employee/tableEmployee.tsx
+++ frontend/src/pages/auth/realizaProfile/employee/tableEmployee.tsx
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/button";
 import { useUser } from "@/context/user-provider";
 import { ip } from "@/utils/ip";
 import axios from "axios";
-import { Eye, Settings2, User } from "lucide-react";
+import { Eye, Search, User } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Blocks } from "react-loader-spinner";
 
 interface TableEmployeeProps {
   idProvider: string | null;
@@ -15,6 +15,8 @@ interface TableEmployeeProps {
 export function TableEmployee({ idProvider }: TableEmployeeProps) {
   const [employees, setEmployee] = useState([]);
   const { user } = useUser();
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para o termo de busca
+  const [isLoading, setIsLoading] = useState(false);
   // const [branches, setBranches] = useState([]);
   // const [selectedBranch, setSelectedBranch] = useState("");
   // const { client } = useClient();
@@ -37,7 +39,7 @@ export function TableEmployee({ idProvider }: TableEmployeeProps) {
   // };
 
   const getEmployee = async () => {
-    // console.log("idProvider watch:", idProvider);
+    setIsLoading(true)
     try {
       const res = await axios.get(
         `${ip}/employee?idSearch=${idProvider}&enterprise=SUPPLIER`,
@@ -53,7 +55,19 @@ export function TableEmployee({ idProvider }: TableEmployeeProps) {
       setEmployee(res.data.content);
     } catch (error) {
       console.log("Erro ao buscar colaboradores:", error);
+    } finally {
+      setIsLoading(false)
     }
+  };
+
+  const filteredEmployees = employees.filter((employee: any) =>
+    `${employee.name} ${employee.surname}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase()),
+  );
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
   };
 
   console.log("colaboradores da branch:", employees);
@@ -73,78 +87,119 @@ export function TableEmployee({ idProvider }: TableEmployeeProps) {
 
   if (user?.role === "ROLE_SUPPLIER_RESPONSIBLE" && "ROLE_SUPPLIER_MANAGER") {
     return (
-      <div className="flex flex-wrap gap-5 justify-center">
-        {employees.map((employee: any) => (
-          <div className="bg-white flex items-start border border-neutral-200 shadow-lg rounded-md">
-            <div className="flex w-[20vw] flex-col gap-9  p-10">
-              <div className="flex items-center justify-between">
-                <h1 className="text-[20px]">Contratos</h1>
-                <Button className="bg-realizaBlue">
-                  Adicionar contratos +
-                </Button>
-              </div>
-              <div className="flex flex-col gap-5">
-                <div className="flex items-center justify-between">
-                  <h2 className="text-realizaBlue font-medium">
-                    Industria Ultra gás - altura
-                  </h2>
-                  <div>
-                    <Eye />
-                  </div>
-                </div>
-                <div className="flex items-center justify-between">
-                  <h2 className="text-realizaBlue font-medium">
-                    Industria Ultra gás - altura
-                  </h2>
-                  <div>
-                    <Eye />
-                  </div>
-                </div>
-              </div>
-              <Button className="bg-realizaBlue">Ver todos os contratos</Button>
-            </div>
-            <div key={employee.idEmployee} className="p-4 w-[15vw]">
-              <div className="flex flex-col gap-5">
-              <div className="flex gap-2">
-                <div className="bg-neutral-200 p-2 rounded-full"><User /></div>
-                <div>
-                  <h3 className="font-medium">{employee.name} {employee.surname}</h3>
-                  <p className="text-sky-950 underline text-[12px]">{employee.position}</p>
-                </div>
-              </div>
-              <div className="flex flex-col gap-5">
-                <span className="text-sky-950 font-medium text-[14px]">Informações do colaborador</span>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>Nome completo:</p>
-                  <p>{employee.name} {employee.surname}</p>
-                </div>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>Status:</p>
-                  <p>{employee.situation}</p>
-                </div>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>Tipo de contrato:</p>
-                  <p>{employee.contractType}</p>
-                </div>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>CBO:</p>
-                  <p>{employee.cbo}</p>
-                </div>
-              </div>
-            </div>
-            </div>
+      <div className="flex flex-col items-center justify-center gap-5">
+        <div className="flex items-center gap-2">
+          <span>Filtrar colaborador</span>
+          <div className="flex w-[50vw] items-center gap-1 rounded-md border border-neutral-500 bg-white p-2">
+            <Search />
+            <input
+              type="text"
+              placeholder="Pesquisar por nome"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="bg-transparent outline-none"
+            />
           </div>
-        ))}
+        </div>
+        {isLoading ? (
+          <div className="flex w-[80vw] flex-wrap justify-center gap-5 rounded-md bg-white p-10 shadow-lg">
+            <Blocks
+              height="80"
+              width="80"
+              color="#4fa94d"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+              visible={true}
+            />
+          </div>
+        ) : (
+          <div className="flex w-[80vw] flex-wrap justify-center gap-5 rounded-md bg-white p-10 shadow-lg">
+            {filteredEmployees.map((employee: any) => (
+              <div className="flex items-start rounded-md border border-neutral-200 bg-white shadow-xl">
+                <div className="flex w-[20vw] flex-col gap-9 p-10">
+                  <div className="flex items-center justify-between">
+                    <h1 className="text-[20px]">Contratos</h1>
+                    <Button className="bg-realizaBlue">
+                      Adicionar contratos +
+                    </Button>
+                  </div>
+                  <div className="flex flex-col gap-5">
+                    <div className="flex items-center justify-between">
+                      <h2 className="text-realizaBlue font-medium">
+                        Industria Ultra gás - altura
+                      </h2>
+                      <div>
+                        <Eye />
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <h2 className="text-realizaBlue font-medium">
+                        Industria Ultra gás - altura
+                      </h2>
+                      <div>
+                        <Eye />
+                      </div>
+                    </div>
+                  </div>
+                  <Button className="bg-realizaBlue">
+                    Ver todos os contratos
+                  </Button>
+                </div>
+                <div key={employee.idEmployee} className="w-[15vw] p-4">
+                  <div className="flex flex-col gap-5">
+                    <div className="flex gap-2">
+                      <div className="rounded-full bg-neutral-200 p-2">
+                        <User />
+                      </div>
+                      <div>
+                        <h3 className="font-medium">
+                          {employee.name} {employee.surname}
+                        </h3>
+                        <p className="text-[12px] text-sky-950 underline">
+                          {employee.position}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-5">
+                      <span className="text-[14px] font-medium text-sky-950">
+                        Informações do colaborador
+                      </span>
+                      <div className="flex items-center gap-1 text-[14px]">
+                        <p>Nome completo:</p>
+                        <p>
+                          {employee.name} {employee.surname}
+                        </p>
+                      </div>
+                      <div className="flex items-center gap-1 text-[14px]">
+                        <p>Status:</p>
+                        <p>{employee.situation}</p>
+                      </div>
+                      <div className="flex items-center gap-1 text-[14px]">
+                        <p>Tipo de contrato:</p>
+                        <p>{employee.contractType}</p>
+                      </div>
+                      <div className="flex items-center gap-1 text-[14px]">
+                        <p>CBO:</p>
+                        <p>{employee.cbo}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
 
   if (user?.role === "ROLE_CLIENT_RESPONSIBLE" && "ROLE_CLIENT_MANAGER") {
     return (
-      <div className="flex flex-wrap gap-5 justify-center">
+      <div className="flex flex-wrap justify-center gap-5">
         {employees.map((employee: any) => (
-          <div className="bg-white flex items-start border border-neutral-200 shadow-lg rounded-md">
-            <div className="flex w-[20vw] flex-col gap-9  p-10">
+          <div className="flex items-start rounded-md border border-neutral-200 bg-white shadow-lg">
+            <div className="flex w-[20vw] flex-col gap-9 p-10">
               <div className="flex items-center justify-between">
                 <h1 className="text-[20px]">Contratos</h1>
                 <Button className="bg-realizaBlue">
@@ -171,35 +226,45 @@ export function TableEmployee({ idProvider }: TableEmployeeProps) {
               </div>
               <Button className="bg-realizaBlue">Ver todos os contratos</Button>
             </div>
-            <div key={employee.idEmployee} className="p-4 w-[15vw]">
+            <div key={employee.idEmployee} className="w-[15vw] p-4">
               <div className="flex flex-col gap-5">
-              <div className="flex gap-2">
-                <div className="bg-neutral-200 p-2 rounded-full"><User /></div>
-                <div>
-                  <h3 className="font-medium">{employee.name} {employee.surname}</h3>
-                  <p className="text-sky-950 underline text-[12px]">{employee.position}</p>
+                <div className="flex gap-2">
+                  <div className="rounded-full bg-neutral-200 p-2">
+                    <User />
+                  </div>
+                  <div>
+                    <h3 className="font-medium">
+                      {employee.name} {employee.surname}
+                    </h3>
+                    <p className="text-[12px] text-sky-950 underline">
+                      {employee.position}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-5">
+                  <span className="text-[14px] font-medium text-sky-950">
+                    Informações do colaborador
+                  </span>
+                  <div className="flex items-center gap-1 text-[14px]">
+                    <p>Nome completo:</p>
+                    <p>
+                      {employee.name} {employee.surname}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-1 text-[14px]">
+                    <p>Status:</p>
+                    <p>{employee.situation}</p>
+                  </div>
+                  <div className="flex items-center gap-1 text-[14px]">
+                    <p>Tipo de contrato:</p>
+                    <p>{employee.contractType}</p>
+                  </div>
+                  <div className="flex items-center gap-1 text-[14px]">
+                    <p>CBO:</p>
+                    <p>{employee.cbo}</p>
+                  </div>
                 </div>
               </div>
-              <div className="flex flex-col gap-5">
-                <span className="text-sky-950 font-medium text-[14px]">Informações do colaborador</span>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>Nome completo:</p>
-                  <p>{employee.name} {employee.surname}</p>
-                </div>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>Status:</p>
-                  <p>{employee.situation}</p>
-                </div>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>Tipo de contrato:</p>
-                  <p>{employee.contractType}</p>
-                </div>
-                <div className="flex items-center gap-1 text-[14px]">
-                  <p>CBO:</p>
-                  <p>{employee.cbo}</p>
-                </div>
-              </div>
-            </div>
             </div>
           </div>
         ))}
@@ -208,50 +273,58 @@ export function TableEmployee({ idProvider }: TableEmployeeProps) {
   }
 
   return (
-    <div className="flex flex-wrap gap-5 justify-center">
-        {employees.map((employee: any) => (
-          <div className="bg-white flex items-start border border-neutral-200 shadow-lg rounded-md">
-            <div className="flex w-[20vw] flex-col gap-9  p-10">
-              <div className="flex items-center justify-between">
-                <h1 className="text-[20px]">Contratos</h1>
-                <Button className="bg-realizaBlue">
-                  Adicionar contratos +
-                </Button>
-              </div>
-              <div className="flex flex-col gap-5">
-                <div className="flex items-center justify-between">
-                  <h2 className="text-realizaBlue font-medium">
-                    Industria Ultra gás - altura
-                  </h2>
-                  <div>
-                    <Eye />
-                  </div>
-                </div>
-                <div className="flex items-center justify-between">
-                  <h2 className="text-realizaBlue font-medium">
-                    Industria Ultra gás - altura
-                  </h2>
-                  <div>
-                    <Eye />
-                  </div>
-                </div>
-              </div>
-              <Button className="bg-realizaBlue">Ver todos os contratos</Button>
+    <div className="flex flex-wrap justify-center gap-5">
+      {employees.map((employee: any) => (
+        <div className="flex items-start rounded-md border border-neutral-200 bg-white shadow-lg">
+          <div className="flex w-[20vw] flex-col gap-9 p-10">
+            <div className="flex items-center justify-between">
+              <h1 className="text-[20px]">Contratos</h1>
+              <Button className="bg-realizaBlue">Adicionar contratos +</Button>
             </div>
-            <div key={employee.idEmployee} className="p-4 w-[15vw]">
-              <div className="flex flex-col gap-5">
-              <div className="flex gap-2">
-                <div className="bg-neutral-200 p-2 rounded-full"><User /></div>
+            <div className="flex flex-col gap-5">
+              <div className="flex items-center justify-between">
+                <h2 className="text-realizaBlue font-medium">
+                  Industria Ultra gás - altura
+                </h2>
                 <div>
-                  <h3 className="font-medium">{employee.name} {employee.surname}</h3>
-                  <p className="text-sky-950 underline text-[12px]">{employee.position}</p>
+                  <Eye />
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <h2 className="text-realizaBlue font-medium">
+                  Industria Ultra gás - altura
+                </h2>
+                <div>
+                  <Eye />
+                </div>
+              </div>
+            </div>
+            <Button className="bg-realizaBlue">Ver todos os contratos</Button>
+          </div>
+          <div key={employee.idEmployee} className="w-[15vw] p-4">
+            <div className="flex flex-col gap-5">
+              <div className="flex gap-2">
+                <div className="rounded-full bg-neutral-200 p-2">
+                  <User />
+                </div>
+                <div>
+                  <h3 className="font-medium">
+                    {employee.name} {employee.surname}
+                  </h3>
+                  <p className="text-[12px] text-sky-950 underline">
+                    {employee.position}
+                  </p>
                 </div>
               </div>
               <div className="flex flex-col gap-5">
-                <span className="text-sky-950 font-medium text-[14px]">Informações do colaborador</span>
+                <span className="text-[14px] font-medium text-sky-950">
+                  Informações do colaborador
+                </span>
                 <div className="flex items-center gap-1 text-[14px]">
                   <p>Nome completo:</p>
-                  <p>{employee.name} {employee.surname}</p>
+                  <p>
+                    {employee.name} {employee.surname}
+                  </p>
                 </div>
                 <div className="flex items-center gap-1 text-[14px]">
                   <p>Status:</p>
@@ -267,9 +340,9 @@ export function TableEmployee({ idProvider }: TableEmployeeProps) {
                 </div>
               </div>
             </div>
-            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+    </div>
   );
 }
