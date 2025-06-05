b7eac91824f71b7950cf721e793c10dbd81d5bde
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-06-05 18:28:14 -0300

    corrections document contract routes

--- api/src/main/java/bl/tech/realiza/usecases/interfaces/documents/contract/CrudDocumentContract.java
+++ api/src/main/java/bl/tech/realiza/usecases/interfaces/documents/contract/CrudDocumentContract.java
@@ -1,26 +1,9 @@
 package bl.tech.realiza.usecases.interfaces.documents.contract;
 
-import bl.tech.realiza.gateways.requests.documents.client.DocumentClientRequestDto;
-import bl.tech.realiza.gateways.requests.documents.contract.DocumentContractRequestDto;
-import bl.tech.realiza.gateways.responses.documents.DocumentResponseDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartFile;
+import bl.tech.realiza.gateways.responses.documents.ContractDocumentAndEmployeeResponseDto;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 public interface CrudDocumentContract {
-    DocumentResponseDto save(DocumentContractRequestDto documentContractRequestDto, MultipartFile file) throws IOException;
-    Optional<DocumentResponseDto> findOne(String id);
-    Page<DocumentResponseDto> findAll(Pageable pageable);
-    Optional<DocumentResponseDto> update(String id, DocumentContractRequestDto documentContractRequestDto, MultipartFile file) throws IOException;
-    void delete(String id);
-    Optional<DocumentResponseDto> upload(String id, MultipartFile file) throws IOException ;
-    Page<DocumentResponseDto> findAllByContract(String idSearch, Pageable pageable);
-    DocumentResponseDto findAllSelectedDocuments (String id);
-    String updateRequiredDocuments(String id, List<String> documentCollection);
-    String addRequiredDocument(String idEnterprise, String documentMatrixId);
-    void removeRequiredDocument(String documentId);
+    ContractDocumentAndEmployeeResponseDto getDocumentAndEmployeeByContractId(String id);
 }
