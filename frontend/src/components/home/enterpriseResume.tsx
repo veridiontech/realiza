0fefdcd4692ecf46d89d38e4dc4e6954ff0ed0c1
leviutima <levi.utima@gmail.com>
2025-06-19 22:13:32 -0300

    refactor: change style if client is null in enterprise resume home

--- frontend/src/components/home/enterpriseResume.tsx
+++ frontend/src/components/home/enterpriseResume.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight } from "lucide-react";
+import { BriefcaseBusiness, ChevronRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "../ui/button";
@@ -11,6 +11,34 @@ export function EnterpriseResume() {
   const { client } = useClient();
   const { selectedBranch } = useBranch();
   
+  if(!client) {
+    return(
+      <div className="dark:bg-primary flex  items-start justify-between rounded bg-white p-4 shadow md:flex-row">
+      <div className="flex items-center gap-3">
+        <div className="bg-neutral-300 p-5 rounded-md">
+          <BriefcaseBusiness className="text-neutral-600" size={50}/>
+        </div>
+        <div className="flex flex-col gap-1">
+            <h3 className="text-lg font-semibold text-realizaBlue">Selecione um cliente</h3>
+            <p className="text-neutral-400 w-[20vw] text-[14px]">
+              é necessário selecionar um cliente, para que suas informações apareçam no dashboard
+            </p>
+        </div>
+      </div>
+      {/* <div className="flex items-center gap-2">
+        <ModalCreateCliente />
+        <>
+          <Button
+            variant={"ghost"}
+            className="dark:bg-primary-foreground rounded-full bg-neutral-200 w-[2vw] p-2 text-zinc-600 hover:bg-neutral-300"
+          >
+            <ChevronRight size={24} className="dark:text-white" />
+          </Button>
+        </>
+      </div> */}
+    </div>
+    )
+  }
 
   return (
     <div className="dark:bg-primary flex w-full items-start justify-between rounded bg-white p-4 shadow md:flex-row">
