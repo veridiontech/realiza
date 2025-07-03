25e4f8ed0f15c10a77c6c045ed3f1dc6c10f9711
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-07-03 15:00:01 -0300

    add additional info to IA validation prompt

---
+++ api/src/main/java/bl/tech/realiza/gateways/repositories/services/IaAdditionalPromptRepository.java
@@ -0,0 +1,7 @@
+package bl.tech.realiza.gateways.repositories.services;
+
+import bl.tech.realiza.domains.services.IaAdditionalPrompt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IaAdditionalPromptRepository extends JpaRepository<IaAdditionalPrompt, String> {
+}
--- api/src/main/java/bl/tech/realiza/usecases/impl/documents/CrudIaAdditionalPromptImpl.java
+++ api/src/main/java/bl/tech/realiza/usecases/impl/documents/CrudIaAdditionalPromptImpl.java
@@ -1,6 +1,10 @@
 package bl.tech.realiza.usecases.impl.documents;
 
+import bl.tech.realiza.domains.documents.matrix.DocumentMatrix;
 import bl.tech.realiza.domains.services.IaAdditionalPrompt;
+import bl.tech.realiza.exceptions.NotFoundException;
+import bl.tech.realiza.gateways.repositories.documents.matrix.DocumentMatrixRepository;
+import bl.tech.realiza.gateways.repositories.services.IaAdditionalPromptRepository;
 import bl.tech.realiza.gateways.requests.services.iaAdditionalPrompt.IaAdditionalPromptRequestDto;
 import bl.tech.realiza.gateways.responses.services.iaAditionalPrompt.IaAdditionalPromptNameListResponseDto;
 import bl.tech.realiza.gateways.responses.services.iaAditionalPrompt.IaAdditionalPromptResponseDto;
@@ -13,37 +17,69 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CrudIaAdditionalPromptImpl implements CrudIaAdditionalPrompt {
+    private final DocumentMatrixRepository documentMatrixRepository;
+    private final IaAdditionalPromptRepository iaAdditionalPromptRepository;
+
     @Override
     public IaAdditionalPromptResponseDto save(IaAdditionalPromptRequestDto requestDto) {
-        return null;
+        return toDto(iaAdditionalPromptRepository.save(toEntity(requestDto)));
     }
 
     @Override
     public IaAdditionalPromptResponseDto update(String id, IaAdditionalPromptRequestDto requestDto) {
-        return null;
+        IaAdditionalPrompt additionalPrompt = iaAdditionalPromptRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Prompt not found"));
+
+        additionalPrompt.setDescription(requestDto.getDescription());
+
+        return toDto(iaAdditionalPromptRepository.save(additionalPrompt));
     }
 
     @Override
-    public IaAdditionalPromptResponseDto delete(String id) {
-        return null;
+    public void delete(String id) {
+        iaAdditionalPromptRepository.deleteById(id);
     }
 
     @Override
     public IaAdditionalPromptResponseDto findById(String id) {
-        return null;
+        return toDto(iaAdditionalPromptRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Prompt not found")));
     }
 
     @Override
     public List<IaAdditionalPromptResponseDto> findAll() {
-        return List.of();
+        return toDto(iaAdditionalPromptRepository.findAll());
     }
 
     @Override
     public List<IaAdditionalPromptNameListResponseDto> findAllNameList() {
-        return List.of();
+        return iaAdditionalPromptRepository.findAll().stream().map(
+                iaAdditionalPrompt -> IaAdditionalPromptNameListResponseDto.builder()
+                        .id(iaAdditionalPrompt.getId())
+                        .documentTitle(iaAdditionalPrompt.getDocumentMatrix().getName())
+                        .build()
+        ).toList();
+    }
+
+    private IaAdditionalPrompt toEntity(IaAdditionalPromptRequestDto requestDto) {
+        DocumentMatrix documentMatrix = documentMatrixRepository.findById(requestDto.getDocumentId())
+                .orElseThrow(() -> new NotFoundException("Document Matrix not found"));
+        return IaAdditionalPrompt.builder()
+                .description(requestDto.getDescription())
+                .documentMatrix(documentMatrix)
+                .build();
     }
 
     private IaAdditionalPromptResponseDto toDto(IaAdditionalPrompt iaAdditionalPrompt) {
-        return null;
+        return IaAdditionalPromptResponseDto.builder()
+                .id(iaAdditionalPrompt.getId())
+                .description(iaAdditionalPrompt.getDescription())
+                .documentId(iaAdditionalPrompt.getDocumentMatrix().getIdDocument())
+                .documentTitle(iaAdditionalPrompt.getDocumentMatrix().getName())
+                .build();
+    }
+
+    private List<IaAdditionalPromptResponseDto> toDto(List<IaAdditionalPrompt> iaAdditionalPrompt) {
+        return iaAdditionalPrompt.stream().map(this::toDto).toList();
     }
 }
