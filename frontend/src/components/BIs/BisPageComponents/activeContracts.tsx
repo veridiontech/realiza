69017b15db9aed330645c14e29d7e931e6318f1c
v.valim_ <victorvalimdev@gmail.com>
2025-07-04 16:21:22 -0300

    file user

--- frontend/src/components/BIs/BisPageComponents/activeContracts.tsx
+++ frontend/src/components/BIs/BisPageComponents/activeContracts.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import { FileUser } from "lucide-react";
+import { Files } from "lucide-react";
 
 interface ActiveContractProps {
   count?: number;
@@ -32,7 +32,7 @@ export function ActiveContracts({ count }: ActiveContractProps) {
           alignContent: "center",
         }}
       >
-        {/* <FileUser height={80} width={70} color="#cccccc" /> */}
+        <Files height={80} width={70} color="#cccccc" />
       </div>
       <div
         style={{
