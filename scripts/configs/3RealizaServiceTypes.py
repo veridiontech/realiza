8ff1dc2653005bd2e6e40d15e4b649fd4d072ed0
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-08-20 17:34:14 -0300

    correction spring security cors

--- scripts/configs/3RealizaServiceTypes.py
+++ scripts/configs/3RealizaServiceTypes.py
@@ -5,20 +5,20 @@ import os
 from datetime import datetime
 
 # Configurações de conexão com o banco de dados MySQL
-# DB_CONFIG = {
-#     "host": "35.184.183.88",
-#     "port": 3306,
-#     "user": "veridion-admin-user",
-#     "password": "uMsgC-x+uAA]yRG1",
-#     "database": "realiza_mysql_development"
-# }
 DB_CONFIG = {
-    "host": "177.170.30.9",
-    "port": 8004,
-    "user": "veridion_user",
-    "password": "SenhaSegura123!",
-    "database": "dbrealiza"
+    "host": "35.184.183.88",
+    "port": 3306,
+    "user": "veridion-admin-user",
+    "password": "uMsgC-x+uAA]yRG1",
+    "database": "realiza_mysql_development"
 }
+# DB_CONFIG = {
+#     "host": "177.170.30.9",
+#     "port": 8004,
+#     "user": "veridion_user",
+#     "password": "SenhaSegura123!",
+#     "database": "dbrealiza"
+# }
 
 # Mapeamento de risco para números
 RISK_MAP = {
