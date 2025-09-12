372aa1c3fda7db7a0a8c6093875da23524cde665
Henrique Barrios <henriquepbarrios2@gmail.com>
2025-09-12 08:46:38 -0300

    api de desenvolvimento

--- frontend/src/utils/ip.ts
+++ frontend/src/utils/ip.ts
@@ -1,2 +1,2 @@
-export const ip = "https://realiza.onrender.com"
-// export const ip = "https://realiza-api-development.onrender.com"
+// export const ip = "https://realiza.onrender.com"
+export const ip = "https://realiza-api-development.onrender.com"
