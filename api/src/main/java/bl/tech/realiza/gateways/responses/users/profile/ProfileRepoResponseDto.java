0ae3b0730e5cde3344af7561cd0d79d495249e8b
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-10-02 12:50:37 -0300

    bugfix permissions in profile response

--- api/src/main/java/bl/tech/realiza/gateways/responses/users/profile/ProfileRepoResponseDto.java
+++ api/src/main/java/bl/tech/realiza/gateways/responses/users/profile/ProfileRepoResponseDto.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.*;
 
-import java.util.List;
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,18 +15,5 @@ public class ProfileRepoResponseDto {
     private String name;
     private String description;
     private Boolean admin;
-    private Boolean viewer;
-    private Boolean manager;
-    private Boolean inspector;
-    private Boolean documentViewer;
-    private Boolean registrationUser;
-    private Boolean registrationContract;
-    private Boolean laboral;
-    private Boolean workplaceSafety;
-    private Boolean registrationAndCertificates;
-    private Boolean general;
-    private Boolean health;
-    private Boolean environment;
-    private Boolean concierge;
     private String clientId;
 }
